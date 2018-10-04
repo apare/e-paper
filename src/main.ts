5eb9c455dfb4b295ae80763066795546213ea79a
Alexandre Paré <alex.pare.inc@gmail.com>
2018-10-03 20:16:35 -0400

    test draw

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ var display = new Display();
 
 async function main() {
   await display.init();
+
+  const canvas = display.getCanvas();
+
+  const ctx = canvas.getContext("2d");
+
+  if (ctx) {
+    ctx.moveTo(10, 10);
+    ctx.lineTo(50, 50);
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  await display.draw(canvas);
 }
 
 main()
